c52d1dfef7e2e9510a08b706718f66503d6bb347
marcus-andre <marcus-andre@hotmail.com>
2025-09-20 23:32:56 +0100

    Add while loop for user to input and define the constant BOARD_SIZE

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#import module "random" from python library. This module provides functions to generate random numbers.
+import random
+
+
+while True:
+    BOARD_SIZE = int(input("\nChose the \033[31mSIZE\033[0m of the board from \033[31m5-10\033[0m: "))
+    if 5 <= BOARD_SIZE <= 10:
+        print("\n---------------------------------------")
+        print(f"The \033[31mSIZE\033[0m of the Board is \033[31m{BOARD_SIZE}\033[0m:")
+        print("---------------------------------------")
+        break
+    else:
+        print("---------------------------------------")
+        print("The \033[31mSIZE\033[0m of the board should be from \033[31m5-10\033[0m:")
+        print("---------------------------------------")
+
+
+NUM_SHIPS = 3
+SCORES = {"player": 0, "computer": 0}
+
+
